a149d6c8bbb05acf4bf73cd0dee442a9df29e9b6
George Soares <george.soares509@gmail.com>
2021-03-21 11:19:27 -0300

    home-semi-completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/global/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContainerComponent } from './components/home/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    ContainerComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/container/container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.sass']
+})
+export class ContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
